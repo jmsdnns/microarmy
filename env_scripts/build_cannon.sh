72fe282060bffbd0314b0467d57525e41c874a8a
James Dennis <jd@j2labs.net>
2011-09-14 22:28:24 -0400

    Removed unnecessary zmq stuff from cannons

--- env_scripts/build_cannon.sh
+++ env_scripts/build_cannon.sh
@@ -16,23 +16,3 @@ apt-get -y install \
     python-pip \
     siege
 
-apt-get install cython
-pip install cython
-
-### Build zeromq from source
-git clone https://github.com/zeromq/libzmq.git
-cd libzmq
-git checkout v2.1.0
-./autogen.sh
-./configure && make && make install
-ldconfig
-cd ..
-
-### Install pyzmq from source
-git clone https://github.com/zeromq/pyzmq.git
-cd pyzmq
-git checkout v2.0.10
-cp setup.cfg.template setup.cfg
-python ./setup.py install
-cd ..
-
